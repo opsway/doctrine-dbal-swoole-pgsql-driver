7ee6399ea98c4afd64c3ba7682cb7832c87eb24f
Aleksey Anokhin <alano@opsway.com>
2022-03-22 16:23:41 +0200

    Added examples

---
+++ example/cli.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+require_once 'vendor/autoload.php';
+
+$connectionParams = [
+    'dbname' => 'mydb',
+    'user' => 'user',
+    'password' => 'secret',
+    'host' => 'dbhost',
+    'driverClass' => \OpsWay\Doctrine\DBAL\Swoole\PgSQL\Driver::class,
+    'poolSize' => 5, // MAX count connections in one pool
+    'tickFrequency' => 60000, // when need check possibilities downscale (close) opened connection to DB in pools
+    'connectionTtl' => 60000, // when connection not used this time - it will be close (free)
+    'usedTimes' => 100, // 1 connection (in pool) will be re-used maximum N queries
+    'retry' => [
+        'max_attempts' => 2, // if connection in pool was timeout (before use) then try re-connect
+        'delay' => 1, // after this time
+    ]
+];
+$pool = (new \OpsWay\Doctrine\DBAL\Swoole\PgSQL\ConnectionPullFactory())($connectionParams);
+$configuration = new \Doctrine\DBAL\Configuration();
+$configuration->setMiddlewares(
+    [new \OpsWay\Doctrine\DBAL\Swoole\PgSQL\DriverMiddleware($pool)]
+);
+$connFactory = static fn() => \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $configuration);
+
+Co\run(function () use ($connFactory) {
+    for ($i = 1; $i <= 5; $i++) { // get 5 connection and make 5 async calls (2 SQL queries in each connection)
+        go(static function () use ($connFactory, $i) {
+            $conn = $connFactory();
+            $v = $conn->fetchOne('SELECT version()');
+            echo $v . PHP_EOL;
+            $conn->fetchOne('SELECT pg_sleep(1)');
+            echo 'routine #: ' . $i . PHP_EOL;
+            defer(static fn() => $conn->close());
+        });
+    }
+    // If repeat this again 50 times then each 5 connection will have same connection to DB by 100 queries count
+    // 500 SQL for 50sec (instead of 250sec)
+});
+
